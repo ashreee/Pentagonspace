f9602d9f4a11ad68acd5247f156ea4235b49e197
ashreee <ashreeash4@gmail.com>
2024-06-23 20:58:43 +0530

    Pattern printing programs added

---
+++ ZeroOneSeq.java
@@ -0,0 +1,14 @@
+public class ZeroOneSeq {
+    public static void main(String[] args) {
+        int n = 5;
+        int k = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+
+                System.out.print(k % 2 + " ");
+                k++;
+            }
+            System.out.println();
+        }
+    }
+}
